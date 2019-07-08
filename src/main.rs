ccab1bb0ff7a8d4ec4b106a9a0b77a18de581c0b
Tormod G. Hellen <tormod@mbr-targeting.com>
2019-07-08 17:34:22 +0200

    Print dmesg.

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ extern crate chrono;
 use sysinfo::{ProcessExt, SystemExt};
 use std::{thread, time};
 use chrono::{DateTime, Utc};
+use std::process::Command;
 
 fn main() {
     println!("Hello, world!");
@@ -27,6 +28,19 @@ fn main() {
         println!("  used swap   : {} kB", system.get_used_swap());
 
         thread::sleep(tenth_of_a_second);
+
+        let maybe_output = Command::new("dmesg").arg("--human").arg("--ctime").arg("--decode").arg("--nopager").output();
+        //dmesg --human -T -x
+        match maybe_output {
+            Err(e) => println!("Could not read from dmesg: {}", e),
+            Ok(output) => {
+                if !output.status.success() {
+                    println!("dmesg failed with error: {:?}", output.stderr)
+                } else {
+                    println!("Read from dmesg: {:?}", output.stdout)
+                }
+            },
+        }
     }
 
     
